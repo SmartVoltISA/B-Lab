a4355ae775b50af37c2306a63f117c417d85bae3
SmartVoltISA <aries72bull@gmail.com>
2026-08-15 21:53:36 +0500

    test: protect binary zero state and self-loop

--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,8 +1,17 @@
-from lab.comparator import adjacency_matrix, compare_relations, relation_from_transitions
+from LAB.comparator import adjacency_matrix, compare_relations, relation_from_transitions
 
 
 def test_relation_is_derived_from_observed_transitions():
     assert relation_from_transitions([0, 1, 0]) == {(0, 1), (1, 0)}
+
+
+def test_zero_self_loop_is_preserved():
+    assert relation_from_transitions([0, 0]) == {(0, 0)}
+
+
+def test_zero_and_one_are_distinct_states():
+    assert relation_from_transitions([0, 1]) == {(0, 1)}
+    assert relation_from_transitions([1, 0]) == {(1, 0)}
 
 
 def test_exact_relation_is_match():
@@ -24,3 +33,7 @@ def test_unexpected_pair_is_difference():
 
 def test_adjacency_matrix_is_deterministic():
     assert adjacency_matrix({(0, 1), (1, 0)}) == [[0, 1], [1, 0]]
+
+
+def test_zero_row_and_column_are_not_lost():
+    assert adjacency_matrix({(0, 0)}) == [[1, 0], [0, 0]]
